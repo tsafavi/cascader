92c98bcf8986502d215f732ee2ad881a3b459e1d
Tara Safavi <taras@allenai.org>
2022-05-26 11:55:00 -0700

    Update cascade run scripts

--- cascade_full.sh
+++ cascade_full.sh
@@ -7,5 +7,5 @@ if [ "$DATASET" == "fb15k-237" ] || [ "$DATASET" == "wn18rr" ]; then
     exit 1
 fi
 
-python src/main.py ${DATASET}/t1/config.yaml --search  # run reranking at tier 1
-python src/main.py ${DATASET}/t2/config.yaml --search  # run reranking at tier 2
+python src/main.py out/${DATASET}/t1/config.yaml --search  # run reranking at tier 1
+python src/main.py out/${DATASET}/t2/config.yaml --search  # run reranking at tier 2
--- cascade_pruned.sh
+++ cascade_pruned.sh
@@ -2,6 +2,6 @@
 set -e
 DATASET=$1
 
-python src/main.py ${DATASET}/t1/config.yaml --search  # run reranking at tier 1
-python src/main.py ${DATASET}/t1_prune/config.yaml  # run answer pruning at tier 1
-python src/main.py ${DATASET}/t2_prune/config.yaml --search  # run reranking at tier 2
+python src/main.py out/${DATASET}/t1/config.yaml --search  # run reranking at tier 1
+python src/main.py out/${DATASET}/t1_prune/config.yaml  # run answer pruning at tier 1
+python src/main.py out/${DATASET}/t2_prune/config.yaml --search  # run reranking at tier 2
