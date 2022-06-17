a4c10c567a5fbddf7c0e0f9af86f37097a69b7d3
Tara Safavi <tsafavi@umich.edu>
2022-06-17 12:59:03 -0700

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -2,11 +2,6 @@
 set -e
 DATASET=$1
 
-if [ "$DATASET" == "fb15k-237" ] || [ "$DATASET" == "wn18rr" ]; then
-    echo "${DATASET} not yet uploaded to S3"
-    exit 1
-fi
-
 curl https://ai-s2-cascader.s3.us-west-2.amazonaws.com/${DATASET}.zip -o out/${DATASET}/models.zip
 cd out/${DATASET}/
 unzip models.zip
